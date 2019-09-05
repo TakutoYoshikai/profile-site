0e50263565835e2d24d89e16b427ce9221ebf220
takuto <takuto.yoshikai@gmail.com>
2019-09-05 14:27:30 +0900

    add en

---
+++ en.js
@@ -0,0 +1,91 @@
+var isEntered = [
+  false,
+];
+
+var msgs = [
+  "My name is Takuto Yoshikai. an university student.",
+  "My hobby is finding cafes. I have secret cafes.",
+  "I love anime!",
+  "I am good at using PC!",
+  "Be my friend!",
+  "Facebook: "
+];
+
+var images = [
+  "./images/takuto.jpg",
+  "./images/cafe.jpg",
+  "./images/anime.jpg",
+  "./images/pc.jpg",
+  "./images/qr.jpg",
+  "./images/facebook.jpg"
+
+];
+
+function addChar(index) {
+  if (msgs[index] == "") {
+    clearInterval(timers[index]);
+    if (index == 4) {
+      document.getElementById("msg" + index).innerHTML += "<br><br><a href='http://line.naver.jp/ti/p/Y-sPE4erWR'><img src='" + images[index] + "' class='img'></a>";
+    } else if (index == 5) {
+      document.getElementById("msg" + index).innerHTML += "<br><br><a href='https://m.facebook.com/qr?id=100008255985123'><img src='" + images[index] + "' class='img'></a>";
+    } else {
+      document.getElementById("msg" + index).innerHTML += "<br><br><img src='" + images[index] + "' class='img'>";
+    }
+    return;
+  }
+  var ch = msgs[index][0];
+  msgs[index] = msgs[index].slice(1);
+  document.getElementById("msg" + index).innerHTML += ch;
+}
+
+var timers = [
+  null,
+  null,
+  null,
+  null,
+  null,
+  null
+];
+
+window.onload = function() {
+    timers[0] = setInterval(function() {
+      addChar(0);
+    }, 100);
+    isEntered[0] = true;
+}
+window.onscroll = function(e) {
+  var scrollHeight = document.documentElement.scrollHeight - (document.body.scrollTop || document.documentElement.scrollTop)- document.documentElement.clientHeight;
+  console.log(scrollHeight);
+  if (scrollHeight < 2000 && !isEntered[1]) {
+    timers[1] = setInterval(function() {
+      addChar(1);
+    }, 100);
+    isEntered[1] = true;
+
+  }
+  if (scrollHeight < 1500 && !isEntered[2]) {
+    timers[2] = setInterval(function() {
+      addChar(2);
+    }, 100);
+    isEntered[2] = true;
+  } 
+  if (scrollHeight < 1000 && !isEntered[3]) {
+    timers[3] = setInterval(function() {
+      addChar(3);
+    }, 100);
+    isEntered[3] = true;
+  }
+  if (scrollHeight < 500 && !isEntered[4]) {
+    timers[4] = setInterval(function() {
+      addChar(4);
+    }, 100);
+    isEntered[4] = true;
+
+  } 
+  if (scrollHeight < 90 && !isEntered[5]) {
+    timers[5] = setInterval(function() {
+      addChar(5);
+    }, 100);
+    isEntered[5] = true;
+  } 
+}
